52d000811f3980e81629773c4cf978d1511330f8
sachi <sachi@LAP-16>
2022-01-01 22:45:29 +0530

    Add project files.

---
+++ exampal/Infrastructure/exampalContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace exampal.Infrastructure
+{
+    public class exampalContext : DbContext
+    {
+        public exampalContext(DbContextOptions<exampalContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
